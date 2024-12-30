6b9905e31c9db54b37879a3817e3df7a9b27a11a
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-12-30 16:58:23 -0500

    Fixed the file structure

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app'
-
-import '../styles/globle.css'
- 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
