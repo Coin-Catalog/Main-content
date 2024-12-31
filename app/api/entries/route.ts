58ce9610a94fd4fbd65c4ed26f7844fb17dac2e5
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-12-31 14:12:07 -0500

    Made the coins display on the home page

--- app/api/entries/route.ts
+++ app/api/entries/route.ts
@@ -1,38 +1,43 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import { NextResponse } from 'next/server';
 
-const postsDirectory = path.join(process.cwd(), 'entries')
+const postsDirectory = path.join(process.cwd(), 'entries');
 
 function getSortedEntriesData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '')
+  const coins = [];
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const categories = fs.readdirSync(postsDirectory);
+  
+  for (let i = 0; i < categories.length; i++) {
+    const catPath = path.join(process.cwd(), `entries/${categories[i]}`)
+    const fileNames = fs.readdirSync(catPath);
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents)
+    const allPostsData = fileNames.map(fileName => {
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, '')
+  
+      // Read markdown file as string
+      const fullPath = path.join(catPath, fileName)
+      const fileContents = fs.readFileSync(fullPath, 'utf8')
+  
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents)
+  
+      // Combine the data with the id
+      return {
+        id,
+        ...matterResult.data
+      }
+    })
 
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data
+    for (let j = 0; j < allPostsData.length; j++) {
+      coins.push(allPostsData[j]);
     }
-  })
-  // Sort posts by date
-  return allPostsData.sort((b, a) => {
-    if (a.id < b.id) {
-      return 1
-    } else {
-      return -1
-    }
-  })
+  }
+
+  return coins;
 }
 
 export async function GET() {
