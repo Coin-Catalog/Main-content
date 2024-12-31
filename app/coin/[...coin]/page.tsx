f15f8072e0f86a754d42fee7d22acc4c036d9507
codefactor-io <support@codefactor.io>
2024-12-31 03:44:01 +0000

    [CodeFactor] Apply fixes to commit c6c580e

--- app/coin/[...coin]/page.tsx
+++ app/coin/[...coin]/page.tsx
@@ -1,87 +1,120 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { headers } from 'next/headers';
+import Head from "next/head";
+import Link from "next/link";
+import { headers } from "next/headers";
 
 //import RelatedCoins  from '../../lib/components/relatedCoins'
 
 //import styles from '../../styles/entry.module.css'
 
-const styles = {}
+const styles = {};
 
 interface EntryDataInterface {
-  "Designer": string,
-  "Mints": string[],
-  "contentHtml": string,
-  "datesMinted": string,
-  "id": string,
-  "mintage": string,
-  "obverse": string,
-  "related": string[],
-  "reverse": string,
-  "title": string,
-  "codeTitle": string,
-  "full": string,
-  "entryData": any
+  Designer: string;
+  Mints: string[];
+  contentHtml: string;
+  datesMinted: string;
+  id: string;
+  mintage: string;
+  obverse: string;
+  related: string[];
+  reverse: string;
+  title: string;
+  codeTitle: string;
+  full: string;
+  entryData: any;
 }
 
 export default async function Page({
-    params,
-  }: {
-    params: Promise<{ coin: string }>
-  }) {
-    const headerList = headers();
-    const domainName = (await headerList).get("host");
-    const catagory = (await params).coin[0];
-    const coin = (await params).coin[1];
+  params,
+}: {
+  params: Promise<{ coin: string }>;
+}) {
+  const headerList = headers();
+  const domainName = (await headerList).get("host");
+  const catagory = (await params).coin[0];
+  const coin = (await params).coin[1];
 
-    let entryData: EntryDataInterface;
+  let entryData: EntryDataInterface;
 
-    try {
-        const response = await fetch(`https://${domainName}/api/coins/${catagory}/${coin}`);
-        const json: any = response.json();
+  try {
+    const response = await fetch(
+      `https://${domainName}/api/coins/${catagory}/${coin}`,
+    );
+    const json: any = response.json();
 
-        entryData = json["data"];
-    } catch (e) {
-        const response = await fetch(`http://${domainName}/api/coins/${catagory}/${coin}`);
-        const json: any = response.json();
+    entryData = json["data"];
+  } catch (e) {
+    const response = await fetch(
+      `http://${domainName}/api/coins/${catagory}/${coin}`,
+    );
+    const json: any = response.json();
 
-        entryData = json["data"];
-    }
-    
+    entryData = json["data"];
+  }
 
-    return (
-        <>
-        <Head>
-            <title>{entryData.title} | US Coin Catalog</title>
+  return (
+    <>
+      <Head>
+        <title>{entryData.title} | US Coin Catalog</title>
 
-            <link rel="apple-touch-icon" sizes="180x180" href="https://raw.githubusercontent.com/Coin-Catalog/Main-content/main/public/images/Favicon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="https://raw.githubusercontent.com/Coin-Catalog/Main-content/main/public/images/Favicon.png" />
+        <link
+          href="https://raw.githubusercontent.com/Coin-Catalog/Main-content/main/public/images/Favicon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="https://raw.githubusercontent.com/Coin-Catalog/Main-content/main/public/images/Favicon.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
 
-            <link rel="canonical" href={`https://coin-catalog.vercel.app/entries/${router.query.id}`} />
-        </Head>
+        <link
+          href={`https://coin-catalog.vercel.app/entries/${router.query.id}`}
+          rel="canonical"
+        />
+      </Head>
 
-        <nav className='no_deceration'>
-            <p className='no_deceration noPadding'><Link href="/" className='no_deceration noPadding'>Home</Link> / <Link href={`/entries/${router.query.id}`} className='no_deceration noPadding'>{router.query.id}</Link></p>
-        </nav>
+      <nav className="no_deceration">
+        <p className="no_deceration noPadding">
+          <Link className="no_deceration noPadding" href="/">
+            Home
+          </Link>{" "}
+          /{" "}
+          <Link
+            className="no_deceration noPadding"
+            href={`/entries/${router.query.id}`}
+          >
+            {router.query.id}
+          </Link>
+        </p>
+      </nav>
 
-        <h1>{entryData.title}</h1>
+      <h1>{entryData.title}</h1>
 
-        <div className={styles.coin_profile}>
-            <div className={styles.profile}>
+      <div className={styles.coin_profile}>
+        <div className={styles.profile}>
+          <picture className={styles.image}>
+            <source className={styles.image} src={entryData.obverse} />
 
-            <picture className={styles.image}>
-            <source src={entryData.obverse} className={styles.image} />
+            <img
+              alt={`Obverse of a ${entryData.title}`}
+              className={styles.image}
+              src={entryData.obverse}
+            />
+          </picture>
 
-            <img src={entryData.obverse} className={styles.image} alt={`Obverse of a ${entryData.title}`} />
-            </picture>
+          <picture className={styles.image}>
+            <source className={styles.image} src={entryData.reverse} />
 
-            <picture className={styles.image}>
-            <source src={entryData.reverse} className={styles.image} />
+            <img
+              alt={`Reverse of a ${entryData.title}`}
+              className={styles.image}
+              src={entryData.reverse}
+            />
+          </picture>
 
-            <img src={entryData.reverse} className={styles.image} alt={`Reverse of a ${entryData.title}`} />
-            </picture>
-
-            <p>
+          <p>
             Dates minted: {entryData.datesMinted}
             <br />
             Mints: {entryData.Mints.join(", ")}
@@ -89,17 +122,17 @@ export default async function Page({
             total minted: {entryData.mintage}
             <br />
             Designer: {entryData.Designer}
-            </p>
-            </div>
-
-            <div className={styles.secondary_part}>
-            <div dangerouslySetInnerHTML={{ __html: entryData.contentHtml }} />
-            </div>
+          </p>
         </div>
 
-        <br />
-        
-        <RelatedCoins relatedCoins={entryData["related"]} />
-        </>
-    )
+        <div className={styles.secondary_part}>
+          <div dangerouslySetInnerHTML={{ __html: entryData.contentHtml }} />
+        </div>
+      </div>
+
+      <br />
+
+      <RelatedCoins relatedCoins={entryData["related"]} />
+    </>
+  );
 }
